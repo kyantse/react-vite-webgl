57ba589f2c14c9cb589509a407b28735a4c2337e
xieqy <jingcheng288@gmail.com>
2025-08-13 18:36:47 +0800

    feat: materials

---
+++ src/pages/GL/lighting/3.1.materials/class/ShaderClass.ts
@@ -0,0 +1,38 @@
+import { mat4, vec3 } from "gl-matrix";
+import { initShaders } from "../../../utils/index.ts";
+
+export default class ShaderClass {
+  gl: WebGL2RenderingContext | null;
+  program: WebGLProgram | null;
+  constructor(
+    gl: WebGL2RenderingContext | null,
+    shader: { vs: string; fs: string }
+  ) {
+    this.program = initShaders(gl, shader.vs, shader.fs) || null;
+    this.gl = gl;
+  }
+
+  use() {
+    if (!this.gl) return;
+    this.gl.useProgram(this.program);
+  }
+
+  setVec3(name: string, value: vec3 | Array<number>) {
+    const position = this.gl?.getUniformLocation(this.program!, name);
+    if (value instanceof Array) {
+      this.gl?.uniform3fv(position!, value);
+    } else {
+      this.gl?.uniform3f(position!, value[0], value[1], value[2]);
+    }
+  }
+
+  setMat4(name: string, value: mat4) {
+    const position = this.gl?.getUniformLocation(this.program!, name);
+    this.gl?.uniformMatrix4fv(position!, false, value);
+  }
+
+  setFloat(name: string, value: GLfloat) {
+    const position = this.gl?.getUniformLocation(this.program!, name);
+    this.gl?.uniform1f(position!, value);
+  }
+}
--- src/pages/gl.tsx
+++ src/pages/gl.tsx
@@ -26,7 +26,8 @@ import { useEffect, useRef } from "react";
 // import Constructor from "./GL/lighting/2.1.basic_lighting_diffuse";
 // import Constructor from "./GL/lighting/2.2.basic_lighting_specular";
 // import Constructor from "./GL/lighting/2.3.basic_lighting_exercise1";
-import Constructor from "./GL/lighting/2.4.basic_lighting_exercise2";
+// import Constructor from "./GL/lighting/2.4.basic_lighting_exercise2";
+import Constructor from "./GL/lighting/3.1.materials";
 
 const GL = () => {
   const contianer = useRef<HTMLCanvasElement | null>(null);
