de261ff37c104f441d13e16dd55ddce597ca9fdf
xieqy <jingcheng288@gmail.com>
2025-08-20 13:43:36 +0800

    feat: multiple lights

---
+++ src/pages/GL/lighting/6.multiple_lights/shader/basic_lighting_glsl.ts
@@ -0,0 +1,93 @@
+export const VSHADER_SOURCE: string = /* glsl */ `#version 300 es
+    layout (location = 0) in vec3 aPos;
+    layout (location = 1) in vec3 aNormal;
+    layout (location = 2) in vec2 aTexCoords;
+
+    out vec3 FragPos;
+    out vec3 Normal;
+    out vec2 TexCoords;
+
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+    uniform mat3 normalMatrix;
+
+    void main()
+    {
+      FragPos = vec3(model * vec4(aPos, 1.0));
+      Normal = normalMatrix * aNormal;  
+      TexCoords = aTexCoords;
+      gl_Position = projection * view * vec4(FragPos, 1.0);
+    }
+    `;
+export const FSHADER_SOURCE: string = /* glsl */ `#version 300 es
+    precision mediump float;
+
+    out vec4 FragColor;
+
+    struct Material {
+        sampler2D diffuse;
+        sampler2D specular;    
+        float shininess;
+    }; 
+
+    struct Light {
+        vec3 position;  
+        vec3 direction;
+        float cutOff;
+        float outerCutOff;
+    
+        vec3 ambient;
+        vec3 diffuse;
+        vec3 specular;
+        
+        float constant;
+        float linear;
+        float quadratic;
+    };
+
+    in vec3 FragPos;  
+    in vec3 Normal;  
+    in vec2 TexCoords;
+      
+    uniform vec3 viewPos;
+    uniform Material material;
+    uniform Light light;
+
+    void main() {
+        // ambient
+        vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+        
+        // diffuse 
+        vec3 norm = normalize(Normal);
+         vec3 lightDir = normalize(light.position - FragPos);
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
+        
+        // specular
+        vec3 viewDir = normalize(viewPos - FragPos);
+        vec3 reflectDir = reflect(-lightDir, norm);  
+        float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+        vec3 specular = light.specular * spec * texture(material.specular, TexCoords).rgb;  
+
+        // spotlight (soft edges)
+        float theta = dot(lightDir, normalize(-light.direction)); 
+        float epsilon = (light.cutOff - light.outerCutOff);
+        float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+        diffuse  *= intensity;
+        specular *= intensity;
+        
+        // attenuation
+        float distance    = length(light.position - FragPos);
+        float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+
+        // ambient  *= attenuation; // remove attenuation from ambient, as otherwise at large distances the light would be darker inside than outside the spotlight due the ambient term in the else branch
+        diffuse   *= attenuation;
+        specular *= attenuation;   
+            
+        vec3 result = ambient + diffuse + specular;
+        FragColor = vec4(result, 1.0);
+    }
+    `;
+
+export default { vs: VSHADER_SOURCE, fs: FSHADER_SOURCE };
